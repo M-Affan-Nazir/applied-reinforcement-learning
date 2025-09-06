ee466adc5971bf654e2600fbad2dfead36515369
M-Affan-Nazir <m.affann80@hotmail.com>
2025-09-06 15:36:09 -0600

    Added select_action() function.

--- bandit_agent.py
+++ bandit_agent.py
@@ -2,7 +2,7 @@ import numpy as np
 
 class BanditAgent:
 
-    def __init__(self, k, alpha=None, c=0, init=0):
+    def __init__(self, k, alpha=None, c=0, init=0, ε = 0.1):
         # k: number of arms
         # alpha: step-size (None -> sample average)
         # c: USB exploration constant ( 0 -> ε-greedy exploration)
@@ -11,8 +11,31 @@ class BanditAgent:
         self.k = k
         self.alpha = alpha
         self.c = c
+        self.ε = ε
         self.Q = np.full(k, init) # Action values
         self.N = np.zeros(k, dtype=int) # N[a] = number of times arm `a` has been chosen
         self.t = 0 # Total time-steps (overall pulls made so far)
     
-   
+    def select_action(self):
+        self.t += 1
+        if self.c == 0:
+            if np.random.rand() <= self.ε:
+                action = np.random.randint(0, self.k)
+                return action
+            else:
+                return np.argmax(self.Q)
+        else:
+            best_preference = np.NINF
+            best_action_index = 0
+            for i in range(self.k):
+                if self.N[i] == 0:
+                    preference = np.inf 
+                    # Sets preference of an untried arm to infinity. 
+                    # This makes sure that the arm is explored atleat once. 
+                else:
+                    preference = self.Q[i] + self.c * np.sqrt(np.log(self.t) / self.N[i])
+                if preference > best_preference:
+                    best_preference = preference
+                    best_action_index = i
+            return best_action_index
+            
